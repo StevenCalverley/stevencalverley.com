e8827b55d4c3aa1551618a319f1b64cfb655112d
Steven <steven@stevencalverley.com>
2018-09-12 11:19:35 +1000

    Added content from resume build

--- inc/templates/header.php
+++ inc/templates/header.php
@@ -10,13 +10,41 @@ if (!isset($title)) {
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Steven Calverley">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+     <!-- Font Awesome -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Typekit Fonts -->
+    <link rel="stylesheet" href="https://use.typekit.net/lqq0iwn.css">
+
+    <!-- Site Styles -->
     <link rel="stylesheet" href="/css/styles.css" >
 
     <title><?php $title ?></title>
   </head>
   <body>
-    <div class="container">
+    <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+    <header>
+      <div class='outer-container'>
+        <nav>
+          <a class="site-logo transition-link" href="/">
+            <span class="logo-text">SC</span>
+          </a>
+          <ul class="nav-right">
+            <li class="list-item"><a class="transition-link" href="/">Home</a></li>
+            <li class="list-item" id="social-click"><a href="javascript:void(0)">Social</a></li>
+            <li class="list-item social-icon" style="display: none;"><a href="https://twitter.com/stevencalverley" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-item social-icon" style="display: none;"><a href="https://dribbble.com/stevencalverley" target="_blank"><i class="fa fa-dribbble"></i></a></li>
+            <li class="list-item social-icon" style="display: none;"><a href="https://github.com/stevencalverley" target="_blank"><i class="fa fa-github"></i></a></li>
+          </ul>
+        </nav>
+     </div>
+    </header>
+    <div class="content">
+
--- public/index.php
+++ public/index.php
@@ -4,9 +4,30 @@
 
 <?php require_once(TEMPLATES_PATH . '/header.php'); ?>
 
-<h1>Steven Calverley</h1>
-<p>Nothing to see here!</p>
+    <section class="inner-container hero">
+      <h1 class="hero-text">Hello, my name is Steven, I am a Web Developer living on the Central Coast, NSW Australia.</h1>
+    </section>
 
+    <section class="outer-container space-bottom">
+        <img src="images/beach.jpg" alt="Steven Hero">
+    </section>
+
+     <section class="inner-container space-bottom">
+      <h3>About</h3>
+      <p>I'm currently work as a full stack Web Developer for the <a href="http://humanservices.gov.au" class="yellow">Department of Human Services</a>, which is where my passion for design and development converge. Ouside of work I spend time with my beautiful wife and two amazing children, often under the sun having a blast.</p>
+      <h3>Services</h3>
+      <h4>Full Stack Web Development</h4>
+      <p>I'm a hard-working web developer with a flair for creating elegant web-based solutions on time and within budget. I build solutions that elevate experiences and empower businesses.</p>
+      <p> I've developed a suite of Performance Reporting web applications supporting government objectives to develop operational staff.</p>
+      <p>Passionate about <strong>software architecture</strong> and <strong>user experience</strong>.</p>
+    </section>
+
+    <section class="cta-section border">
+      <div class="inner-container">
+        <h1>Interested in doing a project together?</h1>
+        <a href="mailto:emailme@stevencalverley.com" class="cta-button">Email Me <span class="arrow-right">→</span></a>
+      </div>
+    </section>
 
 
 <?php require_once(TEMPLATES_PATH . '/footer.php'); ?>
