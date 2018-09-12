c035ce0f5b92e1f7e709403ec020a6f999d38388
Steven <steven@stevencalverley.com>
2018-09-12 11:51:38 +1000

    Add facebook social link

--- inc/templates/header.php
+++ inc/templates/header.php
@@ -50,6 +50,7 @@ if (!isset($title)) {
           <ul class="nav-right">
             <li class="list-item"><a class="transition-link" href="/">Home</a></li>
             <li class="list-item" id="social-click"><a href="javascript:void(0)">Social</a></li>
+            <li class="list-item social-icon" style="display: none;"><a href="https://www.facebook.com/steven.calverley" target="_blank"><i class="fa fa-facebook"></i></a></li>
             <li class="list-item social-icon" style="display: none;"><a href="https://twitter.com/stevencalverley" target="_blank"><i class="fa fa-twitter"></i></a></li>
             <li class="list-item social-icon" style="display: none;"><a href="https://dribbble.com/stevencalverley" target="_blank"><i class="fa fa-dribbble"></i></a></li>
             <li class="list-item social-icon" style="display: none;"><a href="https://github.com/stevencalverley" target="_blank"><i class="fa fa-github"></i></a></li>
