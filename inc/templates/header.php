7dc8456b60f2f0aa83305e5d34b146cca7272548
Steven <steven@stevencalverley.com>
2018-09-12 10:43:39 +1000

    Add bootstrap styles and wrap site in bootstrap container

--- inc/templates/header.php
+++ inc/templates/header.php
@@ -1,9 +1,9 @@
 <?php
-  if (!isset($title)) {
-      $title = 'Steven Calverley';
-  } else {
-      $title = $title . ' | Steven Calverley';
-  }
+if (!isset($title)) {
+    $title = 'Steven Calverley';
+} else {
+    $title = $title . ' | Steven Calverley';
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -13,8 +13,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/styles.css" >
 
-    <title><?= $title ?></title>
+    <title><?php $title ?></title>
   </head>
   <body>
+    <div class="container">
