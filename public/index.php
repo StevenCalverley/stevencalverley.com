805450763971552eeea62a44e359aaa8ea000bcd
Steven <steven@stevencalverley.com>
2018-09-21 11:43:31 +1000

    Updated copy to DHS - Type

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@
 
      <section class="inner-container space-bottom">
       <h3>About</h3>
-      <p>I currently work as a Senior Web Developer for <a href="http://humanservices.gov.au" class="yellow" target="_blank">Department of Humanservices</a>, which is where my passion for design and development converge. Ouside of work I spend time with my beautiful wife and two amazing children, often under the sun having a blast.</p>
+      <p>I currently work as a Senior Web Developer for <a href="http://humanservices.gov.au" class="yellow" target="_blank">Department of Human Services</a>, which is where my passion for design and development converge. Ouside of work I spend time with my beautiful wife and two amazing children, often under the sun having a blast.</p>
       <h3>Services</h3>
       <h4>Full Stack Web Development</h4>
       <p>I'm a hard-working web developer with a flair for creating elegant web-based solutions on time and within budget. I build solutions that elevate experiences and empower businesses.</p>
